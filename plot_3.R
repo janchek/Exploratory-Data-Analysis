ee073e29eb723cf86e9fa57347757fd413c1e3d7
janchek <marijan.mihaldinec@gmail.com>
2017-09-16 02:33:06 +0200

    Add files via upload

---
+++ plot_3.R
@@ -0,0 +1,14 @@
+source('loading data set.R')
+
+## Plot 3
+Sys.setlocale(category = "LC_ALL", locale = "english")
+columnlines <- c("black", "red", "blue")
+labels <- c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+plot(HHPCFINAL$SetTime, HHPCFINAL$Sub_metering_1, type="l", col=columnlines[1], xlab="", ylab="Energy sub metering")
+lines(HHPCFINAL$SetTime, HHPCFINAL$Sub_metering_2, col=columnlines[2])
+lines(HHPCFINAL$SetTime, HHPCFINAL$Sub_metering_3, col=columnlines[3])
+legend("topright", legend=labels, col=columnlines, lty="solid")
+
+## File save
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
