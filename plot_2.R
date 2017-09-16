32214d629d0ddfc828921802e14c4f119af5aea9
janchek <marijan.mihaldinec@gmail.com>
2017-09-16 03:34:04 +0200

    Add files via upload

---
+++ plot_2.R
@@ -0,0 +1,9 @@
+source('loading data set.R')
+
+## Plot 2
+Sys.setlocale(category = "LC_ALL", locale = "english")
+plot(HHPCFINAL$SetTime, HHPCFINAL$GlobalActivePower, type="l", col="black", xlab="", ylab="Global Active Power (kilowatts)")
+
+## File save
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
