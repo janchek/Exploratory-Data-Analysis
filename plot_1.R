f09364eeff64ecdb1d51e9783072517d33f61604
janchek <marijan.mihaldinec@gmail.com>
2017-09-16 03:12:34 +0200

    Delete plot_1.R

--- plot_1.R
+++
@@ -1,8 +0,0 @@
-source('loading data set.R')
-
-## Plot 1
-hist(HHPCFINAL$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
-
-## File save
-dev.copy(png, file="plot1.png", height=480, width=480)
-dev.off()
